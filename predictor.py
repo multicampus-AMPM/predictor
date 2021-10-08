76277cc8ed0408a562f1fcee94d21a489485df1a
inyestar <yuinye@gmail.com>
2021-10-08 14:12:43 +0900

    add function to get data from prometheus

--- predictor.py
+++ predictor.py
@@ -4,8 +4,25 @@ from prometheus_client import Gauge
 from flask import Flask
 from prometheus_flask_exporter import PrometheusMetrics
 import mlflow.sklearn
-import pandas as pd
 import os
+import requests
+
+
+def from_prometheus(query):
+    """ query to prometheus using API """
+    try:
+        # XXX retry count를 지정해야 할지? > retry handler 사용하면 댐
+        response = requests.get(url=os.environ['prom'], params={'query': query}, timeout=10)
+    except Exception as e:
+        # raise NewConnectionError if prometheus cannot be connectd
+        # todo loggoer
+        raise ConnectionError('failed to get from prometheus')
+    metrics = response.json()['data']['result']
+    for metric in metrics:
+        print(metric)
+        # todo smart 데이이터 어케 생겼는지 확인 필요
+        # 지지고 볶고
+    return None
 
 
 app = Flask(__name__)
@@ -37,25 +54,32 @@ def main():
 @app.route('/predict/smart')
 @exporter.do_not_track()
 def predict():
-    # get data from prometheus
-    X = pd.read_json('{"Raw Read Error Rate":{"560631":0.451613},"SpinUpTime":{"560631":1.0},"Reallocated Sector Count":{"560631":1.0},"Seek Error Rate":{"560631":0.536585},"Power on Hours":{"560631":0.052632},"Reported Uncorrectable Error":{"560631":1.0},"High Fly Writes":{"560631":1.0},"Temperature Celsius":{"560631":-0.6},"Hardware ECC Recovered":{"560631":-0.225806},"Current Pending Sector":{"560631":1.0},"Reallocated Sectors Count":{"560631":-1.0},"Current Pending Sectors counts":{"560631":-1.0}}')
-    # get model
+    # X = pd.read_json('{"Raw Read Error Rate":{"560631":0.451613},"SpinUpTime":{"560631":1.0},"Reallocated Sector Count":{"560631":1.0},"Seek Error Rate":{"560631":0.536585},"Power on Hours":{"560631":0.052632},"Reported Uncorrectable Error":{"560631":1.0},"High Fly Writes":{"560631":1.0},"Temperature Celsius":{"560631":-0.6},"Hardware ECC Recovered":{"560631":-0.225806},"Current Pending Sector":{"560631":1.0},"Reallocated Sectors Count":{"560631":-1.0},"Current Pending Sectors counts":{"560631":-1.0}}')
     try:
+        # get data from prometheus
+        dataset = from_prometheus('collectd_memory')
+        if dataset is None:
+            raise ValueError('no data from prometheus')
+        # get model
         model = mlflow.sklearn.load_model(os.path.join(os.environ['repo'], 'smart-model'))
+    except (ValueError, ConnectionError):
+        # TODO logging
+        return 'no recent smart data found'
     except OSError:
         # no such file or directory
         # TODO logging
         return 'no model found'
     # sklearn module required (not imported explicitly)
-    predict_result.set(model.predict(X))
+    predict_result.set(model.predict(dataset))
     return 'ok'
 
 
 def parse_env():
-    # use get function instead of key reference to avoid error
+    # use get function instead of key access to avoid error
     host = os.environ.get('host')
     port = os.environ.get('port')
     repo = os.environ.get('repo')
+    prom = os.environ.get('prom')
 
     # TODO regex validation
     if host is None:
@@ -64,6 +88,9 @@ def parse_env():
         os.environ['port'] = '9106'
     if repo is None:
         os.environ['repo'] = '/ampm/models/'
+    if prom is None:
+        os.environ['prom'] = "prometheus:9090"
+    os.environ['prom'] = f"http://{os.environ['prom']}/api/v1/query"
 
 
 if __name__ == '__main__':
