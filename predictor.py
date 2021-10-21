9b4267fedd3a04872c944c45177fcb38dea85561
inyestar <yuinye@gmail.com>
2021-10-20 20:03:37 -0400

    change method of predict request

--- predictor.py
+++ predictor.py
@@ -1,31 +1,94 @@
 from prometheus_client import Gauge
 from flask import Flask, request
 from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_client.core import GaugeMetricFamily
 import mlflow.sklearn
+import mlflow.xgboost
 import os
 import requests
 import pandas as pd
 
-def from_prometheus(query):
-    """ query to prometheus using API """
-    try:
-        # XXX retry count를 지정해야 할지? > retry handler 사용하면 댐
-        # smart 데이터 두 번 가져와야할 듯
-        response = requests.get(url=os.environ['prom'], params={'query': query}, timeout=10)
-    except Exception as e:
-        # raise NewConnectionError if prometheus cannot be connectd
-        app.logger.error('failed to get from prometheus')
-        raise ConnectionError('failed to get from prometheus')
-    metrics = response.json()['data']['result']
-    result_dict = {}
-    for metric in metrics:
-        result_dict[metric['metric']['type']] = metric['value'][1]
-    return pd.DataFrame([result_dict])
+
+class SmartPredictorExporter(object):
+    
+    def __init__(self, prometheus_url, logger):
+        self.url = prometheus_url
+        self.logger = logger
+        self.name_predict = 'ampm_smart_failure'
+        self.name_predict_proba = 'ampm_smart_failure_proba'
+        self.desc_predict = "Predictor_exporter: 'predict'  Type: 'smart_attribute' Dstype: 'api.Gauge'"
+        self.desc_predict_proba = "Predictor_exporter: 'predict_proba'  Type: 'smart_attribute' Dstype: 'api.Gauge'"
+        self.labels = ['instance', 'model']
+        self.queries = ['collectd_smart_smart_attribute_current', 'collectd_smart_smart_attribute_pretty']
+        # self.models = ['RandomForestClassifier', 'OneClassSVM', 'XGBClassifier']
+        self.models = ['RandomForestClassifier', 'XGBClassifier']
+
+    def collect(self):
+        result = GaugeMetricFamily(name=self.name_predict, documentation=self.desc_predict, labels=self.labels)
+        result_proba = GaugeMetricFamily(name=self.name_predict_proba, documentation=self.desc_predict_proba, labels=self.labels)
+        datasets = self.from_prometheus()
+
+        if datasets is None:
+            datasets = dict()
+
+        # get smart data by instance
+        for instance in datasets:
+            metrics = datasets[instance]
+            smart_data = pd.DataFrame([metrics])
+
+            if smart_data.empty:
+                self.logger.error(f"failed to convert data from '{instance}' to DataFrame")
+                continue
+            # predict failure 
+            for model_name in self.models:
+                try:
+                    ref = f'models:/{model_name}/Production'
+                    if model_name == 'XGBClassifier':
+                        model = mlflow.xgboost.load_model(ref)
+                    else:
+                        model = mlflow.sklearn.load_model(ref)
+                except OSError:
+                    self.logger.error(f"No '{model_name}' model found")
+                    continue
+                predict = model.predict(smart_data)
+                predict_proba = model.predict_proba(smart_data)
+                self.logger.error(predict)
+                self.logger.error(predict_proba)
+                result.add_metric([instance, model_name], predict)
+                result_proba.add_metric([instance, model_name], predict_proba)
+        yield result
+        yield result_proba
+
+    def from_prometheus(self):
+        metrics = list()
+        try:
+            for query in self.queries:
+                response = requests.get(url=self.url, params={'query': query}, timeout=10)
+                metrics.extend(response.json()['data']['result'])
+            if not metrics:
+                raise ValueError('No result from prometheus')
+        except Exception as e:
+            self.logger.error(e)
+            return None
+        
+        instances_smart = dict()
+        # instance 구분
+        for metric in metrics:
+            instance = metric['metric']['instance']
+            smart = instances_smart.get(instance)
+            if smart is None:
+                instances_smart[instance] = dict()
+                smart = instances_smart.get(instance)
+            attribute_name = metric['metric']['type']
+            attribute_name += '-raw' if metric['metric']['__name__'].endswith('pretty') else '-normal'
+            # value = [milliseconds, value]
+            smart[attribute_name] = metric['value'][1]
+        
+        return instances_smart
 
 
 app = Flask(__name__)
 exporter = PrometheusMetrics(app)
-predict_result = Gauge('ampm_predict_result', 'predict result on the reqeust smart data')
 
 
 @app.route('/favicon.ico')
@@ -49,31 +112,6 @@ def main():
     """
 
 
-@app.route('/predict')
-@exporter.do_not_track()
-def predict():
-    app.logger.error(request.args)
-    try:
-        # TODO: retrieve query and model name from request body
-        # get data from prometheus
-        dataset = from_prometheus('collectd_smart_smart_attribute_pretty or collectd_smart_smart_attribute_current')
-        app.logger.error(dataset)
-        if dataset is None or dataset.empty:
-            raise ValueError('no data from prometheus')
-        # get model      
-        model = mlflow.sklearn.load_model('models:/smart-model/Production')
-    except (ValueError, ConnectionError):
-        app.logger.error('no recent smart data found')
-        return 'no recent smart data found'
-    except OSError:
-        app.logger.error('no such file or directory')
-        return 'no model found'
-    # sklearn module required (not imported explicitly)
-    result = model.predict(dataset)
-    predict_result.set(result)
-    return str(result)
-
-
 def parse_env():
     # use get function instead of key access to avoid error
     host = os.environ.get('host')
@@ -87,7 +125,7 @@ def parse_env():
     if port is None:
         os.environ['port'] = '9106'
     if repo is None:
-        os.environ['repo'] = 'model-repository'
+        os.environ['repo'] = 'http://model-repository:5000'
     if prom is None:
         os.environ['prom'] = "prometheus:9090"
     os.environ['prom'] = f"http://{os.environ['prom']}/api/v1/query"
@@ -95,5 +133,6 @@ def parse_env():
 
 if __name__ == '__main__':
     parse_env()
-    mlflow.set_tracking_uri(f"http://{os.environ['repo']}:5000")
+    mlflow.set_tracking_uri(os.environ['repo'])
+    exporter.registry.register(SmartPredictorExporter(os.environ['prom'], app.logger))
     app.run(host=os.environ.get('host'), port=os.environ.get('port'))
