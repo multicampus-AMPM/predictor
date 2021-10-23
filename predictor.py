2c441ccbbd946758c58d4ecc9cb9d8e76f819777
inyestar <yuinye@gmail.com>
2021-10-23 01:18:42 -0400

    change method of loading models

--- predictor.py
+++ predictor.py
@@ -5,6 +5,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_client.core import GaugeMetricFamily
 import mlflow.sklearn
 import mlflow.xgboost
+import mlflow.pyfunc
 import os
 import requests
 import pandas as pd
@@ -13,6 +14,14 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
 
+RF = 'RandomForestClassifier'
+XGB = 'XGBClassifier'
+OCSVM = 'OneClassSVM'
+MODELS = {
+    RF: None,
+    XGB: None,
+    OCSVM: None   
+}
 FEATURES = ['date',
             'serial_number',
             'model',
@@ -149,49 +158,48 @@ class SmartPredictorExporter(object):
         self.name_predict_proba = 'ampm_smart_failure_proba'
         self.desc_predict = "Predictor_exporter: 'predict'  Type: 'smart_attribute' Dstype: 'api.Gauge'"
         self.desc_predict_proba = "Predictor_exporter: 'predict_proba'  Type: 'smart_attribute' Dstype: 'api.Gauge'"
-        self.labels = ['exported_instance', 'model']
+        self.labels = ['exported_instance', 'model', 'drive']
         self.queries = ['collectd_smart_smart_attribute_current', 'collectd_smart_smart_attribute_pretty']
-        self.models = ['RandomForestClassifier', 'XGBClassifier', 'OneClassSVM']
 
     def collect(self):
-        result = GaugeMetricFamily(name=self.name_predict, documentation=self.desc_predict, labels=self.labels)
-        result_proba = GaugeMetricFamily(name=self.name_predict_proba, documentation=self.desc_predict_proba, labels=self.labels)
-        datasets = self.from_prometheus()
+        predict_metric = GaugeMetricFamily(name=self.name_predict, documentation=self.desc_predict, labels=self.labels)
+        predict_proba_metric = GaugeMetricFamily(name=self.name_predict_proba, documentation=self.desc_predict_proba, labels=self.labels)
 
+        datasets = self.from_prometheus()
         if datasets is None:
             datasets = dict()
 
-        # get smart data by instance
-        for instance in datasets:
-            metrics = datasets[instance]
-            smart_data = pd.DataFrame([self.add_features(metrics)])
+        # by exported_instance
+        for instance_name in datasets:
+            drives = datasets[instance_name]
+            # by drive
+            for drive_name in drives:
+                smart_data = pd.DataFrame([self.add_features(drives[drive_name])])
 
-            if smart_data.empty:
-                self.logger.error(f"failed to convert data from '{instance}' to DataFrame")
-                continue
-            # predict failure 
-            for model_name in self.models:
-                try:
-                    ref = f'models:/{model_name}/Production'
-                    model = mlflow.xgboost.load_model(ref) if model_name == 'XGBClassifier' else mlflow.sklearn.load_model(ref)
-                    mode_data = self.cast_data(model_name, smart_data)
-                    predict = model.predict(mode_data)
-                    predict_proba = -1
-                    if model_name == 'XGBClassifier':
-                        predict_proba = predict[0]
-                        predict = 1 if predict_proba > 0.5 else 0
-                    else:
-                        predict = predict[0]
-                        predict_proba = model.predict_proba(mode_data)[0][1]
-                    result.add_metric([instance, model_name], predict)
-                    if predict_proba != -1:
-                        result_proba.add_metric([instance, model_name], predict_proba)
-                except OSError:
-                    self.logger.error(f"No '{model_name}' model found")
-                except (ValueError, AttributeError, TypeError) as e:
-                    self.logger.error(f"'{model_name}' : {e}")
-        yield result
-        yield result_proba
+                if smart_data.empty:
+                    self.logger.error(f"failed to convert data from '{instance_name}/{drive_name}' to DataFrame")
+                    continue
+                
+                self.add_metrics(predict_metric, predict_proba_metric, instance_name, drive_name, smart_data)
+
+        yield predict_metric
+        yield predict_proba_metric
+
+    
+    def add_metrics(self, predict_metric, predict_proba_metric, instance_name, drive_name, smart_data):
+        for model_name in MODELS:
+            model = MODELS[model_name]
+            try:
+                if model is None:
+                    raise AttributeError('Model Not Loaded')
+                mod_data = self.cast_data(model_name, smart_data)
+                result = model.predict(mod_data)[0]
+                predict = result if model == RF else (1 if result > 0.5 else 0)
+                predict_proba = model.predict_proba(mod_data)[0][1] if model == RF else result
+                predict_metric.add_metric([instance_name, model_name, drive_name], predict)
+                predict_proba_metric.add_metric([instance_name, model_name, drive_name], predict_proba)
+            except (ValueError, AttributeError, TypeError, mlflow.exceptions.MlflowException) as e:
+                self.logger.error(f"'{model_name}' : {e}")
 
     def add_features(self, metrics):
         for feature in FEATURES:
@@ -202,9 +210,10 @@ class SmartPredictorExporter(object):
         return metrics
 
     def cast_data(self, model_name, smart_data):
-        if model_name == 'XGBClassifier':
-            return DMatrix(smart_data)
-        elif model_name == 'OneClassSVM':
+        if model_name == XGB:
+            return smart_data
+            # return DMatrix(smart_data)
+        elif model_name == OCSVM:
             return PCA(n_components=1).fit_transform(StandardScaler().fit_transform(smart_data))
         else:
             return smart_data
@@ -222,18 +231,24 @@ class SmartPredictorExporter(object):
             return None
         
         instances_smart = dict()
-        # pc 구분
         for metric in metrics:
             server_name = metric['metric']['instance']
             smart = instances_smart.get(server_name)
             if smart is None:
                 instances_smart[server_name] = dict()
                 smart = instances_smart.get(server_name)
+            
+            drive_name = metric['metric']['smart']
+            drive = smart.get(drive_name)
+            if drive is None:
+                smart[drive_name] = dict()
+                drive = smart.get(drive_name)
+
             attribute_name = metric['metric']['type']
-            attribute_name += '-raw' if metric['metric']['__name__'].endswith('pretty') else '-normal'
+            if not attribute_name.endswith('raw') and not attribute_name.endswith('normal'):
+                attribute_name += '-raw' if metric['metric']['__name__'].endswith('pretty') else '-normal'
             # value = [milliseconds, value]
-            self.logger.error(type(metric['value'][1]))
-            smart[attribute_name] = float(metric['value'][1])        
+            drive[attribute_name] = float(metric['value'][1])    
         return instances_smart
 
 
@@ -281,8 +296,22 @@ def parse_env():
     os.environ['prom'] = f"http://{os.environ['prom']}/api/v1/query"
 
 
+def load_model():
+    # XXX What if
+    # trying to load models faster than registering models in optimizer
+    mlflow.set_tracking_uri(os.environ['repo'])
+    for model_name in MODELS:
+        ref = f'models:/{model_name}/Production'
+        try:
+            # MODELS[model_name] = mlflow.xgboost.load_model(ref) if model_name == XGB else mlflow.sklearn.load_model(ref)
+            MODELS[model_name] = mlflow.pyfunc.load_model(ref)
+        except Exception as e:
+            print(f"'{model_name}' : {e}")
+
+
 if __name__ == '__main__':
     parse_env()
-    mlflow.set_tracking_uri(os.environ['repo'])
+    load_model()
+    # mlflow.set_tracking_uri(os.environ['repo'])
     exporter.registry.register(SmartPredictorExporter(os.environ['prom'], app.logger))
     app.run(host=os.environ.get('host'), port=os.environ.get('port'))
